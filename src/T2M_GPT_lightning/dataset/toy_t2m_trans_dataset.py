cb3756fbca7b224d9afb3312a024e17dc024fcf2
jirayuwat12 <jirayuwat12@gmail.com>
2025-03-18 15:34:31 +0700

    feat: add progress bars for loading skeletons and texts in ToyDataset

--- src/T2M_GPT_lightning/dataset/toy_t2m_trans_dataset.py
+++ src/T2M_GPT_lightning/dataset/toy_t2m_trans_dataset.py
@@ -2,6 +2,7 @@ import clip
 import torch
 from torch import nn
 from torch.utils.data import Dataset
+from tqdm import tqdm
 
 from T2M_GPT_lightning.models.vqvae.vqvae import VQVAEModel
 
@@ -34,7 +35,7 @@ class ToyDataset(Dataset):
     def load_skels(self) -> list[torch.Tensor]:
         skels = []
         with open(self.skels_path, "r") as f:
-            for line in f.readlines():
+            for line in tqdm(f.readlines(), desc="T2M Trans Dataset loading Skeletons"):
                 skel = torch.tensor([float(val) for val in line.strip().split()])
                 skel = skel.reshape(-1, (self.joint_size + self.has_timestamp))[:, : self.joint_size]
                 skels.append(skel)
@@ -42,7 +43,7 @@ class ToyDataset(Dataset):
 
     def load_texts(self) -> list[str]:
         with open(self.text_path, "r") as f:
-            return [text.strip() for text in f.readlines()]
+            return [text.strip() for text in tqdm(f.readlines(), desc="T2M Trans Dataset loading Texts")]
 
     def get_text_features(self, text: str) -> torch.Tensor:
         tokenized_texts = clip.tokenize([text], truncate=True)
